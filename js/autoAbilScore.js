09cbcce37ee93c90c4d777c6ddccbf8daee944c2
Lynsey Nguyen <lynseyn@umich.edu>
2021-12-09 21:20:13 -0500

    added modifier scores and updated checkboxes

--- js/autoAbilScore.js
+++ js/autoAbilScore.js
@@ -1,3 +1,50 @@
+function applyProficiency(id){
+    // add proficiency bonus to id
+    let idnum = parseInt(document.getElementById(id).value);
+    let prof = parseInt(document.getElementById("proficiencyBonus").value);
+    document.getElementById(id).value = idnum + prof;
+}
+
+function fillSkillModifiers() {
+
+}
+
+function strModifier() {
+    let mod = determineModifier(document.getElementById("str").value);
+    document.getElementById("strSavingThrow").value = mod;
+    document.getElementById("athletics").value = mod;
+}
+
+function onChangeAbilityModifier(abil) {
+    let score = parseInt(document.getElementById(abil).value);
+    let modifier = determineModifier(score)
+    let idmodifier = abil + "Modifier";
+    document.getElementById(idmodifier).value = modifier;
+}
+
+function determineModifier(score) {
+    const threshold = [1, 3, 5, 7, 9, 11, 13, 15, 17, 19, 21, 23, 25, 27, 29, 30];
+    modifier = -5;
+    for (let i = 0; i < 16; i++) {
+        if(score <= threshold[i]){
+            if (modifier > 0) {
+                modifier = "+" + modifier;
+            }
+            return modifier;
+        }
+        modifier++;
+    }
+}
+
+function fillBaseMod() {
+    onChangeAbilityModifier("str");
+    onChangeAbilityModifier("dex");
+    onChangeAbilityModifier("con");
+    onChangeAbilityModifier("int");
+    onChangeAbilityModifier("wis");
+    onChangeAbilityModifier("char");
+}
+
 function fillAbilityScore() {
     document.getElementById("str").value = rollAbilityScore();
     document.getElementById("dex").value = rollAbilityScore();
@@ -5,6 +52,8 @@ function fillAbilityScore() {
     document.getElementById("int").value = rollAbilityScore();
     document.getElementById("wis").value = rollAbilityScore();
     document.getElementById("char").value = rollAbilityScore();
+    fillBaseMod();
+    strModifier();
 }
 
 function rollAbilityScore() {
