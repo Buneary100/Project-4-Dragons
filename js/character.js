239bee6cdfc1ff63f7219df7b8d25baec1e14237
Mohammed Alkadmi <malkadmi@umich.edu>
2021-12-10 21:05:21 -0500

    Got login and registration to work

---
+++ js/character.js
@@ -0,0 +1,26 @@
+function start()
+{
+    let characterName = document.querySelector('#characterName');
+    let classLevel = document.querySelector('#classAndLevel');
+    let race = document.querySelector('#race');
+    let background = document.querySelector('#background');
+    let alignment = document.querySelector('#alignment');
+    let playerName = document.querySelector('#playerName');
+    let experiencePoints = document.querySelector('#experiencePoints');
+    let proficiencyBonus = document.querySelector('#proficiencyBonus');
+    let strength = document.querySelector('#str');
+    let strengthMod = document.querySelector('#strModifier');
+    let dexterity = document.querySelector('#dex');
+    let dexterityMod = document.querySelector('#dexModifier');
+    let constitution = document.querySelector('#con');
+    let constitutionMod = document.querySelector('#conModifier');
+    let Intelligence = document.querySelector('#int');
+    let IntelligencenMod = document.querySelector('#intModifier');
+    let Wisdom = document.querySelector('#wis');
+    let WisdomMod = document.querySelector('#wisModifier');
+    let Charisma = document.querySelector('#char');
+    let CharismaMod = document.querySelector('#charModifier');
+}
+
+
+window.onload = start;
