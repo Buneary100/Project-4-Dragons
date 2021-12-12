c52335da3a9bec4edea2588cd88baf505129c998
Mohammed Alkadmi <malkadmi@umich.edu>
2021-12-12 12:59:29 -0500

    Update backend.js

--- js/backend.js
+++ js/backend.js
@@ -123,22 +123,112 @@ app.post('/setProficiency', (req, res) => {
 })
 
 app.post('/save', (req, res) => {
-    const silver = Number(req.body.silver);
-    var sqlCode =`UPDATE charactersheet SET silver = '${silver}' WHERE username = '${app.locals.currentUsers}'`;
-    
-    con.query(sqlCode, (err, result) => {
-        if (err) {
-            console.log(err.message);
-        }
-        if (result.length > 0) {
-            console.log(result.affectedRows);
-            //res.send(JSON.stringify({ message: result[0] }));
+    let sqlCode;
+
+    const firstsetArray = [req.body.characterName, req.body.classLevel,
+    req.body.background, req.body.playerName, req.body.race,
+    req.body.alignment, req.body.hitDice, req.body.otherProficiencies,
+    req.body.features, req.body.equipment, req.body.attackName1,
+    req.body.attackName2, req.body.attackName3, req.body.attackBonus1,
+    req.body.attackBonus2, req.body.attackBonus3, req.body.attackDamage1,
+    req.body.attackDamage2, req.body.attackDamage3];
+
+    const firstsetArrayNames = ["charactername", "class", "background",
+        "playerName", "race", "alignment", "hitDice", "proficiencies",
+        "features", "equipment", "attack1", "attack2", "attack3",
+        "bonus1", "bonus2", "bonus3", "damage1", "damage2", "damage3"];
+
+    const numbersetArray = [Number(req.body.experiencePoints),
+    Number(req.body.proficiencyBonus), Number(req.body.strength),
+    Number(req.body.dexterity), Number(req.body.constitution),
+    Number(req.body.Intelligence), Number(req.body.Wisdom),
+    Number(req.body.Charisma), Number(req.body.hitPointMaximum),
+    Number(req.body.currentHitPoints), Number(req.body.armor),
+    Number(req.body.tempHitPoints), Number(req.body.initiative),
+    Number(req.body.speed), Number(req.body.platinum),
+    Number(req.body.gold), Number(req.body.silver),
+    Number(req.body.copper)];
+
+    const numberSetArrayName = ["experience", "proficiencyBonus",
+        "strength", "dexterity", "constitution", "Intelligence", "Wisdom",
+        "Charisma", "hitPointMaximum", "currentHitPoints", "AC",
+        "tempHitPoints", "initiative", "speed", "platinum", "gold",
+        "silver", "copper"];
+
+    const booleanset = [Boolean(req.body.strengthSave), Boolean(req.body.dexteritySave),
+    Boolean(req.body.constitutionSave), Boolean(req.body.IntelligenceSave),
+    Boolean(req.body.WisdomSave), Boolean(req.body.CharismaSave), Boolean(req.body.athletics),
+    Boolean(req.body.acrobatics), Boolean(req.body.sleightOfHand), Boolean(req.body.stealth),
+    Boolean(req.body.arcane), Boolean(req.body.history), Boolean(req.body.investigation),
+    Boolean(req.body.nature), Boolean(req.body.religion), Boolean(req.body.animalHandling),
+    Boolean(req.body.insight), Boolean(req.body.medicine), Boolean(req.body.perception),
+    Boolean(req.body.survival), Boolean(req.body.deception), Boolean(req.body.intimidation),
+    Boolean(req.body.performance), Boolean(req.body.persuasion)];
+
+    const booleanSetArrayName = ["strengthSave", "dexteritySave",
+        "constitutionSave", "IntelligenceSave", "WisdomSave",
+        "CharismaSave", "athletics", "acrobatics", "sleightOfHand",
+        "stealth", "arcana", "history", "investigation",
+        "nature", "religion", "animalHandling", "insight", "medicine",
+        "perception", "survival", "deception", "intimidation",
+        "performance", "persuasion"];
+
+    for (var i = 0; i < firstsetArray.length; i++) {
+        sqlCode = `UPDATE charactersheet SET ${firstsetArrayNames[i]} = '${firstsetArray[i]}' WHERE username = '${app.locals.currentUsers}'`;
+        con.query(sqlCode, (err, result) => {
+            if (err) {
+                console.log(err.message);
+            }
+            if (result) {
+                console.log(result.affectedRows);
+                //res.send(JSON.stringify({ message: result[0] }));
+            }
+            else {
+                //res.send(JSON.stringify({ message: 'wrong username or password' }));
+            }
+        });
+    }
+
+    for (var i = 0; i < numbersetArray.length; i++) {
+        sqlCode = `UPDATE charactersheet SET ${numbersetArray[i]} = '${numbersetArray[i]}' WHERE username = '${app.locals.currentUsers}'`;
+        con.query(sqlCode, (err, result) => {
+            if (err) {
+                console.log(err.message);
+            }
+            if (result) {
+                console.log(result.affectedRows);
+                //res.send(JSON.stringify({ message: result[0] }));
+            }
+            else {
+                //res.send(JSON.stringify({ message: 'wrong username or password' }));
+            }
+        });
+    }
+
+    for (var i = 0; i < booleanset.length; i++) {
+        
+
+        if(booleanset[i])
+        {
+            sqlCode = `UPDATE proficiency SET ${booleanSetArrayName[i]} = '1' WHERE username = '${app.locals.currentUsers}'`;
         }
         else {
-            //res.send(JSON.stringify({ message: 'wrong username or password' }));
+            sqlCode = `UPDATE proficiency SET ${booleanSetArrayName[i]} = '0' WHERE username = '${app.locals.currentUsers}'`;
         }
-    });
 
+        con.query(sqlCode, (err, result) => {
+            if (err) {
+                console.log(err.message);
+            }
+            if (result) {
+                console.log(result.affectedRows);
+                //res.send(JSON.stringify({ message: result[0] }));
+            }
+            else {
+                //res.send(JSON.stringify({ message: 'wrong username or password' }));
+            }
+        });
+    }
 })
 
 app.listen(portNum, () => {
